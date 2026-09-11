d1b9d3f9d222abd821427197fca7c2c4e74233f3
GitPirates <starshivamverma2@gmail.com>
2026-09-11 22:30:30 +0530

    feat: enhance error handling in api client with ApiClientError class

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,20 +1,42 @@
 import axios from "axios";
 
+export class ApiClientError extends Error {
+    public readonly statusCode: number;
+
+    constructor(statusCode: number, message: string) {
+        super(message);
+
+        this.name = "ApiClientError";
+        this.statusCode = statusCode;
+    }
+}
+
 export const api = axios.create({
-  baseURL: "/api",
-  timeout: 10_000,
-  headers: {
-    "Content-Type": "application/json",
-  },
+    baseURL: "/api",
+    timeout: 10_000,
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 api.interceptors.response.use(
-  (response) => response,
+    (response) => response,
 
-  (error) => {
-    const message =
-      error.response?.data?.error ?? "Something went wrong";
+    (error) => {
+        if (axios.isAxiosError(error)) {
+            const statusCode = error.response?.status ?? 500;
 
-    return Promise.reject(new Error(message));
-  }
+            const message =
+                error.response?.data?.error ??
+                error.response?.data?.message ??
+                error.message ??
+                "Something went wrong";
+
+            return Promise.reject(
+                new ApiClientError(statusCode, message)
+            );
+        }
+
+        return Promise.reject(error);
+    }
 );

2dbbc6655fceca91456ae223e4acab8a80e9f92c
GitPirates <starshivamverma2@gmail.com>
2026-09-12 21:27:58 +0530

    feat: add id field to genre constants for tenrai implementation

--- src/lib/constants/genres.ts
+++ src/lib/constants/genres.ts
@@ -1,25 +1,109 @@
 export const GENRES = [
-    { name: "Action", slug: "action" },
-    { name: "Adventure", slug: "adventure" },
-    { name: "Comedy", slug: "comedy" },
-    { name: "Drama", slug: "drama" },
-    { name: "Fantasy", slug: "fantasy" },
-    { name: "Horror", slug: "horror" },
-    { name: "Mystery", slug: "mystery" },
-    { name: "Romance", slug: "romance" },
-    { name: "Sci-Fi", slug: "sci-fi" },
-    { name: "Slice of Life", slug: "slice-of-life" },
-    { name: "Sports", slug: "sports" },
-    { name: "Supernatural", slug: "supernatural" },
-    { name: "Thriller", slug: "thriller" },
-    { name: "Psychological", slug: "psychological" },
-    { name: "Crime", slug: "crime" },
-    { name: "Historical", slug: "historical" },
-    { name: "Isekai", slug: "isekai" },
-    { name: "Mecha", slug: "mecha" },
-    { name: "Music", slug: "music" },
-    { name: "Military", slug: "military" },
-    { name: "Martial Arts", slug: "martial-arts" },
+    {
+        id: 1,
+        name: "Action",
+        slug: "action",
+    },
+    {
+        id: 2,
+        name: "Adventure",
+        slug: "adventure",
+    },
+    {
+        id: 3,
+        name: "Comedy",
+        slug: "comedy",
+    },
+    {
+        id: 4,
+        name: "Drama",
+        slug: "drama",
+    },
+    {
+        id: 5,
+        name: "Fantasy",
+        slug: "fantasy",
+    },
+    {
+        id: 6,
+        name: "Horror",
+        slug: "horror",
+    },
+    {
+        id: 7,
+        name: "Mystery",
+        slug: "mystery",
+    },
+    {
+        id: 8,
+        name: "Romance",
+        slug: "romance",
+    },
+    {
+        id: 9,
+        name: "Sci-Fi",
+        slug: "sci-fi",
+    },
+    {
+        id: 10,
+        name: "Slice of Life",
+        slug: "slice-of-life",
+    },
+    {
+        id: 11,
+        name: "Sports",
+        slug: "sports",
+    },
+    {
+        id: 12,
+        name: "Supernatural",
+        slug: "supernatural",
+    },
+    {
+        id: 13,
+        name: "Thriller",
+        slug: "thriller",
+    },
+    {
+        id: 14,
+        name: "Psychological",
+        slug: "psychological",
+    },
+    {
+        id: 15,
+        name: "Crime",
+        slug: "crime",
+    },
+    {
+        id: 16,
+        name: "Historical",
+        slug: "historical",
+    },
+    {
+        id: 17,
+        name: "Isekai",
+        slug: "isekai",
+    },
+    {
+        id: 18,
+        name: "Mecha",
+        slug: "mecha",
+    },
+    {
+        id: 19,
+        name: "Music",
+        slug: "music",
+    },
+    {
+        id: 20,
+        name: "Military",
+        slug: "military",
+    },
+    {
+        id: 21,
+        name: "Martial Arts",
+        slug: "martial-arts",
+    },
 ] as const;
 
 // will add more later;

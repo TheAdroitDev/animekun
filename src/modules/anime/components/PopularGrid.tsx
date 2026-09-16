ccbfb6a51a18551d8e39352afd3f92743bf98bce
GitPirates <starshivamverma2@gmail.com>
2026-09-16 12:40:38 +0530

    feat: simplify PopularGrid component by removing unnecessary footer and link

--- src/modules/anime/components/PopularGrid.tsx
+++ src/modules/anime/components/PopularGrid.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import Link from "next/link";
-
 import { usePopular } from "@/modules/anime";
 import { AnimeGrid } from "./AnimeGrid";
 
@@ -9,21 +7,11 @@ export function PopularGrid() {
     const { data: anime, isLoading, error } = usePopular();
 
     return (
-        <div className="popular-grid-section">
-            <AnimeGrid
-                anime={anime}
-                isLoading={isLoading}
-                error={error}
-                skeletonCount={12}
-            />
-
-            {anime && anime.length > 0 && (
-                <div className="popular-grid-footer">
-                    <Link href="/search?sort=popular" className="popular-grid-view-all">
-                        View All Popular →
-                    </Link>
-                </div>
-            )}
-        </div>
+        <AnimeGrid
+            anime={anime}
+            isLoading={isLoading}
+            error={error}
+            skeletonCount={12}
+        />
     );
 }

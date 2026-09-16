94ac7c22f1cfdbc9738e01dafdcb881419f35327
GitPirates <starshivamverma2@gmail.com>
2026-09-16 09:41:56 +0530

    feat: export SeasonalAnime component and useSeasonal hook from index

--- src/modules/anime/index.ts
+++ src/modules/anime/index.ts
@@ -10,7 +10,9 @@ export { AnimeCard } from "./components/AnimeCard";
 export { AnimeCardSkeleton } from "./components/AnimeCardSkeleton";
 export { AnimeGrid } from "./components/AnimeGrid";
 export { PopularGrid } from "./components/PopularGrid";
+export { SeasonalAnime } from "./components/SeasonalAnime";
 export { TrendingCarousel } from "./components/TrendingCarousel";
 export { useTrending } from "./queries/use-trending";
 export { usePopular } from "./queries/use-popular";
+export { useSeasonal, getCurrentSeason } from "./queries/use-seasonal";
 

17b7ab11b3232ac1277ce478afcfcc3c68f64b26
GitPirates <starshivamverma2@gmail.com>
2026-09-26 21:44:42 +0530

    feat: export AnimeDetailHeader component for improved modularity

--- src/modules/anime/index.ts
+++ src/modules/anime/index.ts
@@ -7,6 +7,7 @@ export type {
 } from "./types";
 
 export { AnimeCard } from "./components/AnimeCard";
+export { AnimeDetailHeader } from "./components/AnimeDetailHeader";
 export { AnimeCardSkeleton } from "./components/AnimeCardSkeleton";
 export { AnimeGrid } from "./components/AnimeGrid";
 export { PopularGrid } from "./components/PopularGrid";
